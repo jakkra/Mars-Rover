1b35f960935b1c7df4f7cf0697c452197b39735c
Jakob Krantz <mail@jakobkrantz.se>
2020-05-22 01:09:43 +0200

    Read LoRa from a task instead of in the ISR.

--- src/lora_controller.cpp
+++ src/lora_controller.cpp
@@ -8,6 +8,8 @@
 
 #define MAX_REGISTRATED_CALLBACKS 2
 
+#define LORA_DATA_NOTIFICATION   1
+
 #define LORA_PACKET_LENGTH        12
 #define SCK                       5
 #define MISO                      19
@@ -20,8 +22,8 @@ static const char* TAG = "LORA_CONTROLLER";
 
 static void on_receive_isr(int packetSize);
 
-static portMUX_TYPE isr_data_lock = portMUX_INITIALIZER_UNLOCKED;
 static void lora_state_checker(void* args);
+static void lora_receive_task(void* args);
 
 static LoraControllerStatusCb* status_callbacks[MAX_REGISTRATED_CALLBACKS];
 static uint8_t num_callbacks;
@@ -33,12 +35,12 @@ static uint8_t receive_buf[LORA_PACKET_LENGTH];
 static uint32_t last_lora_data_ticks = 0;
 static uint32_t last_lora_data_checked_ticks = 0;
 
+static TaskHandle_t task_handle;
 
 void lora_controller_init()
 {
   assert(!is_initialized);
   BaseType_t task_create_status;
-  TaskHandle_t task_handle = NULL;
   
   memset(channel_values, 0, sizeof(channel_values));
   num_callbacks = 0;
@@ -60,7 +62,10 @@ void lora_controller_init()
   LoRa.receive(LORA_PACKET_LENGTH);
   ESP_LOGI(TAG, "Starting LoRa OK!");
 
-  task_create_status = xTaskCreate(lora_state_checker, "lora_state_checker", 2048, NULL, tskIDLE_PRIORITY, &task_handle);
+  task_create_status = xTaskCreate(lora_state_checker, "lora_state_checker", 2048, NULL, tskIDLE_PRIORITY, NULL);
+  assert(task_create_status == pdPASS);
+
+  task_create_status = xTaskCreate(lora_receive_task, "lora_receive", 2048, NULL, 2, &task_handle);
   assert(task_create_status == pdPASS);
 
   is_initialized = true;
@@ -78,34 +83,50 @@ uint16_t lora_controller_get_val(uint8_t channel)
 {
   uint16_t value;
   assert(status == LORA_CONTROLLER_CONNECTED);
-  portENTER_CRITICAL(&isr_data_lock);
   assert(channel < RC_NUM_CHANNELS);
   value = channel_values[channel];
-  portEXIT_CRITICAL(&isr_data_lock);
   return value;
+}
+
+static void lora_receive_task(void* args)
+{
+  while (true) {
+    uint32_t i = 0;
+    uint32_t notification;
+    
+    assert(xTaskNotifyWait(LORA_DATA_NOTIFICATION, 0, &notification, portMAX_DELAY));
+    assert(notification == LORA_DATA_NOTIFICATION);
+
+    while (LoRa.available() && i < LORA_PACKET_LENGTH) {
+      receive_buf[i] = (uint8_t)LoRa.read();
+      i++;
+    }
+
+    if (i != LORA_PACKET_LENGTH) {
+      printf("ERROR: Got more data than max packet length\n");
+      return;
+    }
+
+    memcpy(channel_values, receive_buf, LORA_PACKET_LENGTH);
+    //printf("Got: %d, %d \t %d, %d \t %d, %d\n", channel_values[0], channel_values[1], channel_values[2],  channel_values[3], channel_values[4], channel_values[5]);
+  }
 }
 
 static void on_receive_isr(int packet_size)
 {
-  uint16_t i = 0;
   if (packet_size != LORA_PACKET_LENGTH) {
     ets_printf("ERROR: Unexpected packet size %d\n", packet_size);
     return;
   }
 
-  while (LoRa.available() && i < LORA_PACKET_LENGTH) {
-    receive_buf[i] = (uint8_t)LoRa.read();
-    i++;
-  }
-
-  if (i != LORA_PACKET_LENGTH) {
-    ets_printf("ERROR: Got more data than max packet length\n");
-    return;
-  }
-
-  memcpy(channel_values, receive_buf, LORA_PACKET_LENGTH);
   last_lora_data_ticks = xTaskGetTickCountFromISR();
-  //ets_printf("Got: %d, %d \t %d, %d \t %d, %d\n", channel_values[0], channel_values[1], channel_values[2],  channel_values[3], channel_values[4], channel_values[5]);
+
+  BaseType_t shouldWakeUpTask;
+  assert(xTaskNotifyFromISR(task_handle, LORA_DATA_NOTIFICATION, eSetValueWithOverwrite, &shouldWakeUpTask) == pdPASS);
+
+  if (shouldWakeUpTask) {
+    portYIELD_FROM_ISR();
+  }
 }
 
 static void lora_state_checker(void* args)
